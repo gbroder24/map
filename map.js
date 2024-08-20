cc96be6bc121843ce8d38c55b38269e4f6ebdc47
Gary Broderick <garybroderick82@gmail.com>
2024-08-20 20:56:36 +0000

    Initial commit of map method

--- map.js
+++ map.js
@@ -6,15 +6,36 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+
+let results = [];
+
+for(num of nums) {
+  results.push(num * 2);
+}
+
+console.log(results);
 
 // Using map()
 
+const multByTwo = function(num) {
+
+  return num * 2;
+
+}
+
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
+const simplified = nums.map(function(num) {return num * 2});
+console.log(simplified);
 
 // Simplfied w/ map() + arrow function
 
+const simplifiedMap = nums.map(num => num * 2);
+console.log(simplifiedMap);
 
 // With objects:
 const students = [
@@ -37,3 +58,12 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
+console.log(studentsWithIds);
+
+const studentsObject = students.map(student => ({id: student.id, name: student.name}));
+console.log(studentsObject);
+
+const studentsAge = students.map(student => ({student.age = 36}));
+console.log(studentsAge);
